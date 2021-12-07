0b7c258b502d441bc3303f1cbeb579f0fea16ddf
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2021-12-07 18:00:01 +0100

    7. dec

---
+++ 2021/7/b.py
@@ -0,0 +1,22 @@
+positions = [16,1,2,0,4,2,7,1,2,14]
+
+filename = '2021/7/input.txt'
+with open(filename) as file:
+  line = file.read()
+  positions = [int(num) for num in line.split(',')]
+
+
+fuels = []
+for i in range(max(positions)+1):
+  fuel = 0
+  for pos in positions:
+    move = abs(pos - i)
+    for j in range(move + 1):
+      fuel += j
+  fuels.append(fuel)
+
+print(fuels)
+print(min(fuels))
+
+
+  
